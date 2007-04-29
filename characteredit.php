c78593a106a4dcfba333bd36ddac73fb46cb2bcc
(no author) <(no author)@eaed3a89-ab57-494d-94ba-1e47530512d7>
2007-04-29 17:14:15 +0000

    Tons of shit - script now works - needs input validation and more testing FOR THE ZEBRAS!

--- characteredit.php
+++ characteredit.php
@@ -18,34 +18,45 @@ if (!$pcid) {
 	print "<select name=\"pcid\">";
 	$pcsql = mysql_query("SELECT pcid,name FROM playercharacter", $mysql);
 	while (list($pcid, $pcname) = mysql_fetch_row($pcsql)) {
-	        print "<option value=\"$pcid\">$pcname</option>";
+		print "<option value=\"$pcid\">$pcname</option>";
 	}
 	print "</select><br>";
 	print "<br><input type=\"submit\" value=\"Edit\">";
-}
+	}
 
-if ($pcid) {
+if ($pcid && !$save) {
 	// Pull all character data from database
-	$pcsql = mysql_query("select * from playercharacter where pcid=\"$pcid\"");
+	$pcsql = mysql_query("SELECT * FROM playercharacter WHERE pcid=\"$pcid\"");
 	$pchash = mysql_fetch_assoc($pcsql);
 	$pcname = $pchash['name'];
 	$pcplayer = $pchash['player'];
 	$pcactive = $pchash['active'];
 	$pcdate = $pchash['date'];
-	$enterer = $pchash['enterer'];
+	$pcenterer = $pchash['enterer'];
 
-	print "Now editing <b>$pcname</b><br>";
-	print "PCID: <b>$pcid</b><br>";
-	print "Edited: $pcdate by $pcadmin<br><br>";
+	print "<form action=\"characteredit.php\">";
+	print "Now editing <b>$pcname</b> (<a href=\"characteredit.php\">change?</a>)<br>";
+	print "PC-ID: $pcid<br>";
+	print "Edited: $pcdate by $pcenterer<br><br>";
 	print "Name: <input type=\"text\" name=\"pcname\" value=\"$pcname\"><br>";
 	print "Played By: <input type=\"text\" name=\"pcplayer\" value=\"$pcplayer\"><br>";
-//	print "Active? <input type=\"text\" name=\"pcactive\" value=\"$pcactive\"><br>";
+	print "Active? Yes <input type=\"radio\" name=\"pcactive\" value=\"1\" checked>";
+	print " No <input type=\"radio\" name=\"pcactive\" value=\"0\"><br>";
 	print "<input type=\"hidden\" name=\"save\" value=\"1\">";
 	print "<input type=\"hidden\" name=\"pcid\" value=\"$pcid\">";
 	print "<br><input type=\"submit\" value=\"Save\">";
 
 }
-	
+
 if ($pcid && $save && $pcname && $pcplayer && $pcactive) {
-//	$pcsql = mysql_query("UPDATE playercharacter SET name=\"$pcname\", player=\"$pcplayer\", active = \"$pcactive\" WHERE pcid=\"$pcid\"");
+	$pcsql = mysql_query("UPDATE playercharacter SET name=\"$pcname\", player=\"$pcplayer\", active = \"$pcactive\", date = NOW() WHERE pcid=\"$pcid\"");
+	if (!$pcsql) {
+		print "<br><br><b>Somethings not right! Did not save!</b>";
+	} else {
+		print "Saved! <a href=\"characteredit.php\">Do Another?</a>";
+	}
 }
+
+?>
+</body>
+</html>
