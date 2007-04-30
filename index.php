4fb1e4b09e66821c281fe1ba3286a9d2cc2ded25
(no author) <(no author)@eaed3a89-ab57-494d-94ba-1e47530512d7>
2007-04-30 05:14:17 +0000

    Adapted code to use mysql based monster list from SRD

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ $gameid = mysql_real_escape_string($_GET['gameid']);
 $mode = mysql_real_escape_string($_GET['mode']);
 $gameid = mysql_real_escape_string($_GET['gameid']);
 $pcid = mysql_real_escape_string($_GET['pcid']);
-$foe = mysql_real_escape_string($_GET['foe']);
+$foename = mysql_real_escape_string($_GET['foename']);
+$foeid = mysql_real_escape_string($_GET['foeid']);
 $cr = mysql_real_escape_string($_GET['cr']);
 
 if (!$gameid) {
@@ -40,12 +41,12 @@ if ($gameid && !$mode) {
 	}
 	print "</select><br><br>";
 	print "Enemy Killed: ";
-	print "<select name=\"foe\">";
-	print "<option value=\"addnew\">Add A New Foe</option>";
-	$foesql = mysql_query("SELECT DISTINCT foe FROM killtally ORDER BY foe ASC", $mysql);
-	while (list($foe) = mysql_fetch_array($foesql)) {
-		print "<option value=\"$foe\">$foe</option>";
+	print "<select name=\"foeid\">";
+	$foesql = mysql_query("SELECT foeid,name FROM monster ORDER BY name ASC", $mysql);
+	while (list($foeid,$foename) = mysql_fetch_array($foesql)) {
+		print "<option value=\"$foeid\">$foename</option>";
 	}
+//	print "<option value=\"addnew\">Add A New Foe</option>";
 	print "</select>";
 	print "<br>Challenge Rating: ";
 	print "<input type=\"text\" name=\"cr\" maxlength=\"5\">";
@@ -53,27 +54,22 @@ if ($gameid && !$mode) {
 	print "<br><input type=\"submit\" value=\"Kerblewie\">";
 }
 
-if (($mode == 'insert') && $gameid && $pcid && ($foe == 'addnew') && $cr) {
-	print "Name of Foe: ";
-	print "<form action=\"index.php\">";
-	print "<input type=\"text\" name=\"foe\" maxlength=\"50\">";
-	print "<input type=\"hidden\" name=\"mode\" value=\"insert\">";
-	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
-	print "<input type=\"hidden\" name=\"pcid\" value=\"$pcid\">";
-	print "<input type=\"hidden\" name=\"cr\" value=\"$cr\">";
-	print "<br><input type=\"submit\" value=\"Kerblewie\">";
-}
+//if (($mode == 'insert') && $gameid && $pcid && ($foeid == 'addnew') && $cr) {
+//	print "Name of Foe: ";
+//	print "<form action=\"index.php\">";
+//	print "<input type=\"text\" name=\"foe\" maxlength=\"50\">";
+//	print "<input type=\"hidden\" name=\"mode\" value=\"insert\">";
+//	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
+//	print "<input type=\"hidden\" name=\"pcid\" value=\"$pcid\">";
+//	print "<input type=\"hidden\" name=\"cr\" value=\"$cr\">";
+//	print "<br><input type=\"submit\" value=\"Kerblewie\">";
+//}
 
-if (($mode == 'insert') && $gameid && $pcid && ($foe != 'addnew') && $cr) {
-	print "<br>Your entry of: <br>";
-	$enterkill = mysql_query("INSERT INTO killtally (gameid,pcid,foe,challengerating,enterer,date) VALUES (\"$gameid\",\"$pcid\",\"$foe\",\"$cr\",\"$username\",NOW())");
+
+if (($mode == 'insert') && $gameid && $pcid && ($foeid != 'addnew') && $cr) {
+	$enterkill = mysql_query("INSERT INTO killtally (gameid,pcid,foeid,challengerating,enterer,date) VALUES (\"$gameid\",\"$pcid\",\"$foeid\",\"$cr\",\"$username\",NOW())");
 	if ($enterkill) {
-		// Print out the results!
-		$gamesql = mysql_query("SELECT name FROM game WHERE gameid=\"$gameid\"", $mysql); 
-		$gamename = mysql_fetch_row($gamesql);
-		$pcsql = mysql_query("SELECT name FROM playercharacter WHERE pcid=\"$pcid\"", $mysql); 
-		$pcname = mysql_fetch_row($pcsql);
-		print "<br><b>$cr</b> CR points awarded to <b>$pcname[0]</b> for the slaying of a <b>$foe</b> in the game <b>$gamename[0]</b> by <b>$username</b><br>";
+		print "Entered!<br>";
 		print "<a href=\"index.php?gameid=$gameid\">Another One!</a>";
 	} else {
 		print "Didnt get entered, something screwed up";
--- killtally.php
+++ killtally.php
@@ -6,34 +6,36 @@
 <body>
 <?
 
-$foe = mysql_real_escape_string($_GET['foe']);
+$foename = mysql_real_escape_string($_GET['foename']);
+$foeid = mysql_real_escape_string($_GET['foeid']);
 $mode = mysql_real_escape_string($_GET['mode']);
-$oldfoe = mysql_real_escape_string($_GET['oldfoe']);
 
-if (!$foe && !$mode) {
+if (!$foeid && !$mode) {
 	print "Select Foe to Edit: ";
 	print "<form action=\"killtally.php\">";
-	print "<select name=\"foe\">";
-	$foesql = mysql_query("SELECT DISTINCT foe FROM killtally ORDER BY foe ASC", $mysql);
-	while (list($foe) = mysql_fetch_array($foesql)) {
-		print "<option value=\"$foe\">$foe</option>";
+	print "<select name=\"foeid\">";
+	$foesql = mysql_query("SELECT foeid,name FROM monster ORDER BY name ASC", $mysql);
+	while (list($foeid,$foe) = mysql_fetch_array($foesql)) {
+		print "<option value=\"$foeid\">$foe</option>";
 	}
 	print "</select>";
 	print "<input type=\"submit\" value=\"Edit Foe\">";
 }
 
-if ($foe && !$mode) {
+if ($foeid && !$mode) {
 	print "<b>!!!WARNING!!!! THIS WILL CHANGE THE NAME OF ALL THE MONSTERS THAT USE THIS NAME !!!WARNING!!!</b><br>";
 	print "Edit Foe: ";
 	print "<form action=\"killtally.php\">";
-	print "<input type=\"text\" name=\"foe\" value=\"$foe\">";
-	print "<input type=\"hidden\" name=\"oldfoe\" value=\"$foe\">";
+	$foesql = mysql_query("SELECT name FROM monster WHERE foeid=\"$foeid\"", $mysql);
+	$foename = mysql_fetch_array($foesql);
+	print "<input type=\"text\" name=\"foename\" value=\"$foename[0]\">";
+	print "<input type=\"hidden\" name=\"foeid\" value=\"$foeid\">";
 	print "<input type=\"hidden\" name=\"mode\" value=\"update\">";
 	print "<br><input type=\"submit\" value=\"Save Foe\">";
 }
 
-if ($foe && ($mode = 'update') && $oldfoe) {
-	$foesql = mysql_query("UPDATE killtally SET foe=\"$foe\" WHERE foe=\"$oldfoe\"", $mysql);
+if ($foename && ($mode = 'update') && $foeid) {
+	$foesql = mysql_query("UPDATE monster SET name=\"$foename\" WHERE foeid=\"$foeid\"", $mysql);
 	if ($foesql) {
 		print "Success";
 	} else {
