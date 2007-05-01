6721dbbbcbf866d5ac09f275e908ecc74c5da4c8
(no author) <(no author)@eaed3a89-ab57-494d-94ba-1e47530512d7>
2007-05-01 03:58:29 +0000

    Foe -> Monster

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 print "<br><br>";
 print "<a href=\"index.php\">Enter an Encounter</a> ";
 print "<a href=\"characteredit.php\">Character Editor</a> ";
-print "<a href=\"monsteredit.php\">Foe Editor</a> ";
+print "<a href=\"monsteredit.php\">Monster Editor</a> ";
 ?>
