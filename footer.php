66bd39c3bc8aace86e5766bcefb8f936984645f5
(no author) <(no author)@eaed3a89-ab57-494d-94ba-1e47530512d7>
2007-05-07 05:30:31 +0000

    Added gameeditor

--- footer.php
+++ footer.php
@@ -3,4 +3,5 @@ print "<br><br>";
 print "<a href=\"index.php\">Enter an Encounter</a> ";
 print "<a href=\"characteredit.php\">Character Editor</a> ";
 print "<a href=\"monsteredit.php\">Monster Editor</a> ";
+print "<a href=\"gameedit.php\">Game Editor</a> ";
 ?>
