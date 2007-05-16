97373da1e196cef730f9b72d65c3321637627f37
corvus <corvus@eaed3a89-ab57-494d-94ba-1e47530512d7>
2007-05-16 06:58:06 +0000

    Work on eventID and a safe way to reset it.

--- round.php
+++ round.php
@@ -5,22 +5,22 @@
 <body>
 <?php
 
-$gameid = mysql_real_escape_string($_GET['gameid']);
-$eventid = mysql_real_escape_string($_GET['eventid']);
-$mode = mysql_real_escape_string($_GET['mode']);
-$sourceid = mysql_real_escape_string($_GET['sourceid']);
-$actionid = mysql_real_escape_string($_GET['actionid']);
-$spellid = mysql_real_escape_string($_GET['spellid']);
-$itemid = mysql_real_escape_string($_GET['itemid']);
-$destid = $_GET['destid'];
-$hpadj = $_GET['hpadj'];
-$sthrow = $_GET['sthrow'];
-$kill = $_GET['kill'];
+$gameid = mysql_real_escape_string($_POST['gameid']);
+$eventid = mysql_real_escape_string($_POST['eventid']);
+$mode = mysql_real_escape_string($_POST['mode']);
+$sourceid = mysql_real_escape_string($_POST['sourceid']);
+$actionid = mysql_real_escape_string($_POST['actionid']);
+$spellid = mysql_real_escape_string($_POST['spellid']);
+$itemid = mysql_real_escape_string($_POST['itemid']);
+$destid = $_POST['destid'];
+$hpadj = $_POST['hpadj'];
+$sthrow = $_POST['sthrow'];
+$kill = $_POST['kill'];
 
 if (!$gameid) {
         print "Welcome $username!<br><br>";
         print "First off, please select which game you wish to administer: ";
-        print "<form action=\"round.php\">";           // method=\"post\"><br>";
+        print "<form action=\"round.php\" method=\"post\"><br>";
         print "<select name=\"gameid\">";
         $game = mysql_query("SELECT * FROM game where active=\"1\" ORDER BY name ASC", $mysql);
         while (list($gameid, $gamename) = mysql_fetch_row($game)) {
@@ -33,7 +33,7 @@ if (!$gameid) {
 
 if ($gameid) {
 	
-	if (!$eventid) {
+	if (!$eventid || $eventidreset = '1') {
 		$eventid = time();
 	}
 	
@@ -64,11 +64,22 @@ if ($gameid) {
 	}
 	
 	print "Enter hot player-on-player action $username!<br>";
-	print "The eventid is <b>$eventid</b><br>";
+	
+	// EventID with Reset Button
+	print "The eventid is <b>$eventid</b>";
+	print "<form action=\"round.php\" method=\"post\">";
+	print "<input type=\"hidden\" name=\"eventidreset\" value=\"1\">";
+	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
+	print "<input type=\"submit\" value=\"Reset EventID\"><br>";
+	print "</form>";
+
+	// What game are we playing?
         $gamesql = mysql_query("SELECT name FROM game WHERE gameid=\"$gameid\"", $mysql);
         $gamename = mysql_fetch_row($gamesql);
         print "You are currently running <b>$gamename[0]</b> (<a href=\"round.php\">change</a>)<br><br>";
-	print "<form action=\"round.php\">"; // method=\"post\"><br>";
+	
+	// Start the Form of D00m
+	print "<form action=\"round.php\" method=\"post\">";
 	
 	// SOURCE ($sourceid)
 	print "<li>Source: ";
@@ -77,7 +88,7 @@ if ($gameid) {
 	$pcmonlist = "<option value=\"\"> ---Characters---</option>";
 	$pcidsql = mysql_query("SELECT pc.pcid,pc.name,pc.player FROM playercharacter pc JOIN whowhere ww USING(pcid) JOIN game g USING(gameid) WHERE g.gameid = \"$gameid\" ORDER BY pc.name ASC",$mysql);
 	while (list($pcid, $pcname, $pcplayer) = mysql_fetch_row($pcidsql)) {
-		$pcmonlist .= "<option value=\"P-$pcid\">$pcname ($pcplayer)</option>";
+		$pcmonlist .= "<option value=\"P-$pcid\">$pcname ($pcplayer) ($pcid)</option>";
 	}
 	print "<option value=\"\"> </option>";
 	
@@ -86,7 +97,7 @@ if ($gameid) {
 	$pcmonlist .= "<option value=\"\"> </option>";
 	$monidsql = mysql_query("SELECT id,name FROM monster ORDER BY name ASC", $mysql);
 	while (list ($monid,$monname) = mysql_fetch_row($monidsql)) {
-		$pcmonlist .= "<option value=\"M-$monid\">$monname</option>";
+		$pcmonlist .= "<option value=\"M-$monid\">$monname ($monid)</option>";
 	}
 	
 	print "<select name=\"sourceid\">";
@@ -131,57 +142,18 @@ if ($gameid) {
 	for ($i=0; $i<5; $i++) {
 		print "<select name=\"destid[]\"> $pcmonlist </select>";
 	        print "Save Throw: ";
-        	print "<input type=\"radio\" name=\"sthrow[$i]\" value=\"Y\">Made";
-        	print " <input type=\"radio\" name=\"sthrow[$i]\" value=\"N\">Fail";
-		print " | Killed? <input type=\"checkbox\" name=\"kill[$i]\" value=\"Y\"><br>";
-
+        	print "<input type=\"radio\" name=\"sthrow[$i]\" value=\"M\">Made";
+        	print " <input type=\"radio\" name=\"sthrow[$i]\" value=\"F\">Fail";
+		print " | Killed? <input type=\"checkbox\" name=\"kill[$i]\" value=\"Y\">";
+		print " | HP: <input type=\"text\" size=4 name=\"hpadj[$i]\"><br>";
 	}
 	print "<hr>";
-
-	// ------------- //
-	
-	// DAMAGE/HEALED ($hpadj)
-	print "<li>Hit Point Adjustment (Damage/Healed): ";
-	print "<input type=\"text\" name=\"hpadj\">";
-	print "<hr>";
-
-	// ------------ //
-
-	// SAVING THROW ($sthrow)
-	print "<li>Saving Throw:<br> ";
-	print "<input type=\"radio\" name=\"sthrow[0]\" value=\"Y\">Succeeded<br>";
-	print "<input type=\"radio\" name=\"sthrow[0]\" value=\"N\">Failed<br>";
-	
-	print "<input type=\"radio\" name=\"sthrow[1]\" value=\"Y\">Succeeded<br>";
-	print "<input type=\"radio\" name=\"sthrow[1]\" value=\"N\">Failed<br>";
-
-	print "<input type=\"radio\" name=\"sthrow[2]\" value=\"Y\">Succeeded<br>";
-	print "<input type=\"radio\" name=\"sthrow[2]\" value=\"N\">Failed<br>";
-
-
-	print "<input type=\"radio\" name=\"sthrow[3]\" value=\"Y\">Succeeded<br>";
-	print "<input type=\"radio\" name=\"sthrow[3]\" value=\"N\">Failed<br>";
-
-
-	print "<input type=\"radio\" name=\"sthrow[4]\" value=\"Y\">Succeeded<br>";
-	print "<input type=\"radio\" name=\"sthrow[4]\" value=\"N\">Failed<br>";
-
-	
-	print "<input type=\"radio\" name=\"sthrow[5]\" value=\"Y\">Succeeded<br>";
-	print "<input type=\"radio\" name=\"sthrow[5]\" value=\"N\">Failed<br>";
-	
-	// ------------ //
-
-	// Enemy Killed? ($kill)
-	print "<li> Enemy Killed? ";
-	print "<input type=\"checkbox\" name=\"kill\" value=\"Y\">";
-	print "<br><br>";
-
 
 	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
 	print "<input type=\"hidden\" name=\"eventid\" value=\"$eventid\">";
 	print "<input type=\"hidden\" name=\"mode\" value=\"save\">";
 	print "<br><input type=\"submit\" value=\"Enter\">";
 
+	print "</form>";
 }
 ?>
