3549113131d975c54b6971d5884bbcb5a9d3d775
corvus <corvus@eaed3a89-ab57-494d-94ba-1e47530512d7>
2007-05-16 06:29:22 +0000

    *sigh* Doing something I probably shouldnt be doing

--- round.php
+++ round.php
@@ -6,13 +6,16 @@
 <?php
 
 $gameid = mysql_real_escape_string($_GET['gameid']);
+$eventid = mysql_real_escape_string($_GET['eventid']);
+$mode = mysql_real_escape_string($_GET['mode']);
 $sourceid = mysql_real_escape_string($_GET['sourceid']);
+$actionid = mysql_real_escape_string($_GET['actionid']);
 $spellid = mysql_real_escape_string($_GET['spellid']);
 $itemid = mysql_real_escape_string($_GET['itemid']);
 $destid = $_GET['destid'];
-$hpadj = mysql_real_escape_string($_GET['hpadj']);
-$sthrow = mysql_real_escape_string($_GET['sthrow']);
-$kill = mysql_real_escape_string($_GET['kill']);
+$hpadj = $_GET['hpadj'];
+$sthrow = $_GET['sthrow'];
+$kill = $_GET['kill'];
 
 if (!$gameid) {
         print "Welcome $username!<br><br>";
@@ -29,11 +32,39 @@ if (!$gameid) {
 }
 
 if ($gameid) {
-
-
 	
+	if (!$eventid) {
+		$eventid = time();
+	}
 	
-	print "Enter player action!<br><br>";
+	if ($gameid && $eventid && $sourceid && $actionid && $destid && $mode == 'save') {
+		list ($sourcetype,$sid) = split("-",$sourceid);
+		for ($x=0; $x < count($destid); $x++) {
+			$dest = $destid[$x];
+			if ($sthrow) {
+				$st = $sthrow[$x];
+			}
+			if ($kill) {
+				$ded = $kill[$x];
+			}
+			if ($hpadj) {
+				$hp = $hpadj[$x];
+			}
+			list ($desttype,$did) = split("-",$dest);
+			if ($did > 0) {  // Holy shit this is a kludge
+				$enterstat = mysql_query("INSERT INTO stattally (eventid, sourcetype, sourceid, actionid, spellid, itemid, desttype, destid, hpadj, sthrow, destkill, date, enterer) values (\"$eventid\",\"$sourcetype\",\"$sid\",\"$actionid\",\"$spellid\",\"$itemid\",\"$desttype\",\"$did\",\"$hp\",\"$st\",\"$ded\",NOW(),\"$username\")",$mysql);
+			}
+		}
+		if ($enterstat) {
+			print "Success! Do Another!<br>";
+		} else {
+			print "Something went wrong<br>";
+	
+		}
+	}
+	
+	print "Enter hot player-on-player action $username!<br>";
+	print "The eventid is <b>$eventid</b><br>";
         $gamesql = mysql_query("SELECT name FROM game WHERE gameid=\"$gameid\"", $mysql);
         $gamename = mysql_fetch_row($gamesql);
         print "You are currently running <b>$gamename[0]</b> (<a href=\"round.php\">change</a>)<br><br>";
@@ -46,7 +77,7 @@ if ($gameid) {
 	$pcmonlist = "<option value=\"\"> ---Characters---</option>";
 	$pcidsql = mysql_query("SELECT pc.pcid,pc.name,pc.player FROM playercharacter pc JOIN whowhere ww USING(pcid) JOIN game g USING(gameid) WHERE g.gameid = \"$gameid\" ORDER BY pc.name ASC",$mysql);
 	while (list($pcid, $pcname, $pcplayer) = mysql_fetch_row($pcidsql)) {
-		$pcmonlist .= "<option value=\"p-$pcid\">$pcname ($pcplayer)</option>";
+		$pcmonlist .= "<option value=\"P-$pcid\">$pcname ($pcplayer)</option>";
 	}
 	print "<option value=\"\"> </option>";
 	
@@ -55,7 +86,7 @@ if ($gameid) {
 	$pcmonlist .= "<option value=\"\"> </option>";
 	$monidsql = mysql_query("SELECT id,name FROM monster ORDER BY name ASC", $mysql);
 	while (list ($monid,$monname) = mysql_fetch_row($monidsql)) {
-		$pcmonlist .= "<option value=\"m-$monid\">$monname</option>";
+		$pcmonlist .= "<option value=\"M-$monid\">$monname</option>";
 	}
 	
 	print "<select name=\"sourceid\">";
@@ -77,7 +108,7 @@ if ($gameid) {
 		print "<option value=\"$spellid\">$spellname</option>";
 	}
 	print "</select><br>";
-		// Item.. Fuck this is a big list.. :(
+		// Item.. Fuck this is a big list.. :(  Anyway to cache it? Maybe Memcache?
 	print "<input type=\"radio\" name=\"actionid\" value=\"I\"> Item   ";
 	print "<select name=\"itemid\">";
 	print "<option value=\"\">---SELECT ITEM---</option>";
@@ -95,13 +126,17 @@ if ($gameid) {
 
 	// -------------- //
 
-	// DESTINATION ($destid)
+	// DESTINATION ($destid), HPADJUSTMENT ($hpadj), SAVES ($sthrow)
 	print "<li>Destination:<br> ";
 	for ($i=0; $i<5; $i++) {
-		print "<select name=\"destid[]\"> $pcmonlist </select><br>";
+		print "<select name=\"destid[]\"> $pcmonlist </select>";
+	        print "Save Throw: ";
+        	print "<input type=\"radio\" name=\"sthrow[$i]\" value=\"Y\">Made";
+        	print " <input type=\"radio\" name=\"sthrow[$i]\" value=\"N\">Fail";
+		print " | Killed? <input type=\"checkbox\" name=\"kill[$i]\" value=\"Y\"><br>";
+
 	}
 	print "<hr>";
-	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
 
 	// ------------- //
 	
@@ -114,9 +149,26 @@ if ($gameid) {
 
 	// SAVING THROW ($sthrow)
 	print "<li>Saving Throw:<br> ";
-	print "<input type=\"radio\" name=\"sthrow\" value=\"Y\">Succeeded<br>";
-	print "<input type=\"radio\" name=\"sthrow\" value=\"N\">Failed<br>";
-	print "<hr>";
+	print "<input type=\"radio\" name=\"sthrow[0]\" value=\"Y\">Succeeded<br>";
+	print "<input type=\"radio\" name=\"sthrow[0]\" value=\"N\">Failed<br>";
+	
+	print "<input type=\"radio\" name=\"sthrow[1]\" value=\"Y\">Succeeded<br>";
+	print "<input type=\"radio\" name=\"sthrow[1]\" value=\"N\">Failed<br>";
+
+	print "<input type=\"radio\" name=\"sthrow[2]\" value=\"Y\">Succeeded<br>";
+	print "<input type=\"radio\" name=\"sthrow[2]\" value=\"N\">Failed<br>";
+
+
+	print "<input type=\"radio\" name=\"sthrow[3]\" value=\"Y\">Succeeded<br>";
+	print "<input type=\"radio\" name=\"sthrow[3]\" value=\"N\">Failed<br>";
+
+
+	print "<input type=\"radio\" name=\"sthrow[4]\" value=\"Y\">Succeeded<br>";
+	print "<input type=\"radio\" name=\"sthrow[4]\" value=\"N\">Failed<br>";
+
+	
+	print "<input type=\"radio\" name=\"sthrow[5]\" value=\"Y\">Succeeded<br>";
+	print "<input type=\"radio\" name=\"sthrow[5]\" value=\"N\">Failed<br>";
 	
 	// ------------ //
 
@@ -126,6 +178,9 @@ if ($gameid) {
 	print "<br><br>";
 
 
+	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
+	print "<input type=\"hidden\" name=\"eventid\" value=\"$eventid\">";
+	print "<input type=\"hidden\" name=\"mode\" value=\"save\">";
 	print "<br><input type=\"submit\" value=\"Enter\">";
 
 }
