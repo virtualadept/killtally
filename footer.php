f214c3bbf6768a2928a5579aabece1d50e3d38b1
corvus <corvus@eaed3a89-ab57-494d-94ba-1e47530512d7>
2007-05-18 05:47:47 +0000

    Added encounter viewer

--- footer.php
+++ footer.php
@@ -4,4 +4,5 @@ print "<a href=\"index.php\">Enter an Encounter</a> ";
 print "<a href=\"characteredit.php\">Character Editor</a> ";
 print "<a href=\"monsteredit.php\">Monster Editor</a> ";
 print "<a href=\"gameedit.php\">Game Editor</a> ";
+print "<a href=\"encounterview.php\">Encounter Viewer</a> ";
 ?>
