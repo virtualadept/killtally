47201c7c3b605c82cfb5e0aab0a903b1b4c72f11
corvus <corvus@eaed3a89-ab57-494d-94ba-1e47530512d7>
2007-05-18 05:45:12 +0000

    phased out old batch adder for round-based additon

--- index.php
+++ index.php
@@ -6,121 +6,184 @@
 <?php
 
 $gameid = mysql_real_escape_string($_POST['gameid']);
+$eventid = mysql_real_escape_string($_POST['eventid']);
 $mode = mysql_real_escape_string($_POST['mode']);
-$gameid = mysql_real_escape_string($_POST['gameid']);
-$killid = mysql_real_escape_string($_POST['killid']);
-$assistid = $_POST['assistid'];
-$killdamage = $_POST['killdamage'];
-$assistdamage = $_POST['assistdamage'];
-$foename = mysql_real_escape_string($_POST['foename']);
-$foeid = mysql_real_escape_string($_POST['foeid']);
+$sourceid = mysql_real_escape_string($_POST['sourceid']);
+$actionid = mysql_real_escape_string($_POST['actionid']);
+$spellid = mysql_real_escape_string($_POST['spellid']);
+$itemid = mysql_real_escape_string($_POST['itemid']);
 $encounternotes = mysql_real_escape_string($_POST['encounternotes']);
+$destid = $_POST['destid'];
+$hpadj = $_POST['hpadj'];
+$hpadjtype = $_POST['hpadjtype'];
+$sthrow = $_POST['sthrow'];
+$kill = $_POST['kill'];
 
 if (!$gameid) {
-	print "Welcome $username!<br><br>";
-	print "First off, please select which game you wish to administer: ";
-	print "<form action=\"index.php\" method=\"post\"><br>";
-	print "<select name=\"gameid\">";
-	$game = mysql_query("SELECT * FROM game where active=\"1\" ORDER BY name ASC", $mysql);
-	while (list($gameid, $gamename) = mysql_fetch_row($game)) {
-        	print "<option value=\"$gameid\">$gamename</option>";
+        print "Welcome $username!<br><br>";
+        print "First off, please select which game you wish to administer: ";
+        print "<form action=\"round.php\" method=\"post\"><br>";
+        print "<select name=\"gameid\">";
+        $game = mysql_query("SELECT * FROM game where active=\"1\" ORDER BY name ASC", $mysql);
+        while (list($gameid, $gamename) = mysql_fetch_row($game)) {
+                print "<option value=\"$gameid\">$gamename</option>";
+        }
+        print "</select><br><br>";
+        print "<input type=\"submit\" value=\"Go!\">";
+        print "</form>";
+}
+
+if ($gameid) {
+	
+	if (!$eventid || $eventidreset == '1') {
+		$eventid = time();
 	}
-	print "</select><br><br>";
-	print "<input type=\"submit\" value=\"Go!\">";
+	
+	if ($gameid && $eventid && $sourceid && $actionid && $destid && $mode == 'save') {
+		list ($sourcetype,$sid) = split("-",$sourceid);
+		for ($x=0; $x < count($destid); $x++) {
+			$dest = $destid[$x];
+			if ($sthrow) {
+				$st = $sthrow[$x];
+			}
+			if ($kill) {
+				$ded = $kill[$x];
+			}
+			if ($hpadj) {
+				$hp = $hpadj[$x];
+			}
+			if ($hpadjtype) {
+				$hptype = $hpadjtype[$x];
+			}
+			list ($desttype,$did) = split("-",$dest);
+			if ($did > 0) {  // Holy shit this is a kludge
+				$enterstat = mysql_query("INSERT INTO stattally (eventid, gameid, sourcetype, sourceid, actionid, spellid, itemid, desttype, destid, hpadj, hpadjtype, sthrow, destkill, date, enterer) values (\"$eventid\", \"$gameid\", \"$sourcetype\",\"$sid\",\"$actionid\",\"$spellid\",\"$itemid\",\"$desttype\",\"$did\",\"$hp\", \"$hptype\",\"$st\",\"$ded\",NOW(),\"$username\")",$mysql);
+			}
+		}
+		if ($enterstat) {
+			print "Success! Do Another!<br>";
+			print "<hr>";
+		} else {
+			print "Something went wrong<br>";
+	
+		}
+	}
+	
+	if ($encounternotes) {
+		$encountersql = mysql_query("INSERT IGNORE INTO encounternotes (eventid,notes,date) values (\"$eventid\", \"$encounternotes\",NOW())",$mysql);
+	}
+
+	// EventID with Reset Button
+	print "<form action=\"round.php\" method=\"post\">";
+	print "<input type=\"hidden\" name=\"eventidreset\" value=\"1\">";
+	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
+	print "Welcome $username, the EncounterID is <b>$eventid</b> <input type=\"submit\" value=\"New Encounter?\"><br>";
+	print "</form>";
+
+	// What game are we playing?
+        $gamesql = mysql_query("SELECT name FROM game WHERE gameid=\"$gameid\"", $mysql);
+        $gamename = mysql_fetch_row($gamesql);
+        print "You are currently running <b>$gamename[0]</b> (<a href=\"round.php\">change</a>)<br>";
+	
+	// Start the Form of D00m
+	print "<form action=\"round.php\" method=\"post\">";
+
+	// Put the enter button up here so people on laptops dont have to scroll
+	print "<br><input type=\"submit\" value=\"Enter Round Data\"><-- This is here so you dont have to scroll down";
+	print "<hr>";
+
+	// SOURCE ($sourceid)
+	print "<li>Source: ";
+		// Because we'll have to generate this same data about 100 times, save the list in a variable
+		// First, get a list of the characters
+	$pcmonlist = "<option value=\"\"> ---Characters---</option>";
+	$pcidsql = mysql_query("SELECT pc.pcid,pc.name,pc.player FROM playercharacter pc JOIN whowhere ww USING(pcid) JOIN game g USING(gameid) WHERE g.gameid = \"$gameid\" ORDER BY pc.name ASC",$mysql);
+	while (list($pcid, $pcname, $pcplayer) = mysql_fetch_row($pcidsql)) {
+		$pcmonlist .= "<option value=\"P-$pcid\">$pcname ($pcplayer)</option>";
+	}
+	$pcmonlist .= "<option value=\"\"> </option>";
+	
+		// Now lets get the monsters
+	$pcmonlist .= "<option value=\"\"> ---Monsters---</option>";
+	$pcmonlist .= "<option value=\"\"> </option>";
+	$monidsql = mysql_query("SELECT id,name FROM monster ORDER BY name ASC", $mysql);
+	while (list ($monid,$monname) = mysql_fetch_row($monidsql)) {
+		$pcmonlist .= "<option value=\"M-$monid\">$monname</option>";
+	}
+	
+	print "<select name=\"sourceid\">";
+	print "$pcmonlist";
+	print "</select><hr>";
+
+	// ------------- //
+
+	// ACTION ($actionid)
+	print "<li>Method:<br> ";
+		// Attack
+	print "<input type=\"radio\" name=\"actionid\" value=\"A\"> Attack<br>";
+		// Hold Action
+	print "<input type=\"radio\" name=\"actionid\" value=\"H\"> Hold Action<br>";
+		// Spell
+	print "<input type=\"radio\" name=\"actionid\" value=\"S\"> Spell  ";
+	print "<select name=\"spellid\">";
+	print "<option value=\"\">---SELECT SPELL---</option>";
+	$spellsql = mysql_query("SELECT id,name FROM spell ORDER BY name ASC",$mysql);
+	while (list ($spellid,$spellname) = mysql_fetch_row($spellsql)) {
+		print "<option value=\"$spellid\">$spellname</option>";
+	}
+	print "</select><br>";
+		// Item.. Fuck this is a big list.. :(  Anyway to cache it? Maybe Memcache?
+	print "<input type=\"radio\" name=\"actionid\" value=\"I\"> Item   ";
+	print "<select name=\"itemid\">";
+	print "<option value=\"\">---SELECT ITEM---</option>";
+	$itemlist = array("Ring", "Rod", "Staff", "Wonderous", "Artifact", "Potion", "Oil", "Potion, Oil", "Scroll", "Wand", "Cursed");
+	$itemquery = "SELECT id,category,name FROM item WHERE ";
+	foreach ($itemlist as $item) {
+		$itemquery .= "category = '$item' OR ";
+	}
+	$itemquery .= "category = 'Universal Items' ORDER BY name ASC";
+	$itemsql = mysql_query($itemquery, $mysql);
+	while (list ($itemid,$itemcat,$itemname) = mysql_fetch_row($itemsql)) {
+		print "<option value=\"$itemid\">$itemname ($itemcat)</option>";
+	}
+	print "</select><hr>";
+
+	// -------------- //
+
+	// DESTINATION ($destid), HPADJUSTMENT ($hpadj), SAVES ($sthrow), HPTYPE ($hpadjtype)
+	print "<li>Destination:<br> ";
+	for ($i=0; $i<5; $i++) {
+		print "<select name=\"destid[]\"> $pcmonlist </select>";
+	        print "Save: ";
+        	print "<input type=\"radio\" name=\"sthrow[$i]\" value=\"M\">Made";
+        	print " <input type=\"radio\" name=\"sthrow[$i]\" value=\"F\">Fail";
+		print " /|\ HP: <input type=\"text\" size=4 name=\"hpadj[$i]\">";
+        	print " (<input type=\"radio\" name=\"hpadjtype[$i]\" value=\"D\" checked>Damage";
+        	print " <input type=\"radio\" name=\"hpadjtype[$i]\" value=\"H\">Heal)";
+		print " /|\ Killed? <input type=\"checkbox\" name=\"kill[$i]\" value=\"Y\">";
+		print "<br>";
+	}
+	print "<hr>";
+
+
+	// -------------- //
+
+	// Encounter Notes
+        print "Encounter Notes:<br>";
+        print "<textarea name=\"encounternotes\" value=\"$encounternotes\" cols=\"40\" rows=\"6\"></textarea><br>";
+
+
+
+	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
+	print "<input type=\"hidden\" name=\"eventid\" value=\"$eventid\">";
+	print "<input type=\"hidden\" name=\"mode\" value=\"save\">";
+	print "<br><input type=\"submit\" value=\"Enter Round Data\">";
+
 	print "</form>";
 }
 
-if ($gameid && !$mode) {
-	print "Welcome $username!<br> Please make your selection<br>";
-	print "<form action=\"index.php\" method=\"post\"><br>";
-	$gamesql = mysql_query("SELECT name FROM game WHERE gameid=\"$gameid\"", $mysql);
-	$gamename = mysql_fetch_row($gamesql);
-	print "You are currently running <b>$gamename[0]</b> (<a href=\"index.php\">change</a>)<br><br>";
-	print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
-
-	// Character who scored kill
-	print "Character who scored kill:<br>";
-	$charsql = mysql_query("SELECT pc.pcid,pc.name,pc.player FROM playercharacter pc JOIN whowhere ww USING(pcid) JOIN game g USING(gameid) WHERE g.gameid = \"$gameid\" ORDER BY pc.name ASC",$mysql);
-	while (list($pcid,$pcname,$pcplayer) = mysql_fetch_row($charsql)) {
-		print "<li>$pcname ($pcplayer)<input type=radio name=\"killid\" value=\"$pcid\">";
-		print " @ <input type=text size=\"3\" maxlength=\"3\" name=\"killdamage[$pcid]\"> hp<br>";
-	}
-
-	// Characters who assisted
-	print "<hr>";
-	print "Character(s) who assisted:<br>";
-	$assistsql = mysql_query("SELECT pc.pcid,pc.name,pc.player FROM playercharacter pc JOIN whowhere ww USING(pcid) JOIN game g USING(gameid) WHERE g.gameid = \"$gameid\" ORDER BY pc.name ASC",$mysql);
-	while (list($pcid,$pcname,$pcplayer) = mysql_fetch_row($assistsql)) {
-		print "<li>$pcname ($pcplayer)<input type=\"checkbox\" name=\"assistid[]\" value=\"$pcid\">";
-		print " @ <input type=text size=\"3\" maxlength=\"3\" name=\"assistdamage[$pcid]\">hp<br>";
-	}
-	
-	// Who did we all kill?
-	print "<hr>";
-	print "Enemy Killed: ";
-	print "<select name=\"foeid\">";
-	$foesql = mysql_query("SELECT id,name FROM monster ORDER BY name ASC", $mysql);
-	while (list($foeid,$foename) = mysql_fetch_array($foesql)) {
-		print "<option value=\"$foeid\">$foename</option>";
-	}
-	print "</select>";
-	
-	// Notes about the Encounter?
-	print "<hr>";
-	print "Encounter Notes:<br>";
-	print "<textarea name=\"encounternotes\" cols=\"40\" rows=\"6\"></textarea><br>";
-	
-	// Submitola!
-	print "<input type=\"hidden\" name=\"mode\" value=\"insert\">";
-	print "<br><input type=\"submit\" value=\"Enter\">";
-}
-
-
-// Ah shit, here we go with the database magic!
-if (($mode == 'insert') && $gameid && $killid && ($foeid != 'addnew')) {
-	// Sanity checks to make sure killid != assistid
-	if ($assistid) {
-		foreach ($assistid as $foo) {
-			if ($killid == $foo) {
-				print "Cannot Kill and Assist!<br>";
-				exit;
-			}
-		}
-	}
-
-	// Get a nice random unique number to identify all people in one encounter
-	$eventid = time();
-	
-	// Process the kill first
-	$enterkill = mysql_query("INSERT INTO killtally (gameid,pcid,foeid,enterer,date,stat,eventid,damage) VALUES (\"$gameid\",\"$killid\",\"$foeid\",\"$username\",NOW(),\"K\",\"$eventid\",\"$killdamage[$killid]\")");
-	if ($enterkill) {
-		print "Entered!<br>";
-		print "<form action=\"index.php\" method=\"post\">";
-		print "<input type=\"hidden\" name=\"gameid\" value=\"$gameid\">";
-		print "<br><input type=\"submit\" value=\"Do Another?\">";
-	} else {
-		print "Didnt get entered, something screwed up";
-	}
-	
-	// Process the assists after
-	if (count($assistid) >= 1) {
-		for ($x = 0; $x < count($assistid); $x++) {
-			$pcid = $assistid[$x];
-			$enterassist = mysql_query("INSERT INTO killtally (gameid,pcid,foeid,enterer,date,stat,eventid,damage) VALUES (\"$gameid\",\"$pcid\",\"$foeid\",\"$username\",NOW(),\"A\",\"$eventid\",\"$assistdamage[$pcid]\")");
-		}
-	}
-	
-	// Process the encounter notes
-	if ($encounternotes) {
-		$encountersql = mysql_query("INSERT INTO encounternotes (eventid,notes) VALUES (\"$eventid\",\"$encounternotes\")", $mysql);
-	}
-
-}
-
-include "footer.php";
-
 ?>
-<br>
-</form>
+
+<?php include "footer.php" ?>
 </body>
 </html>
